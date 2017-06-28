875dd1b9b1ab7e6ca35703bce531f04d44e58190
snyanglin <261003737@qq.com>
2017-06-28 15:20:49 +0800

    4 在testB中新加了一条打印，看能否上传到github

--- src/main/java/com/tele/maventest/testB.java
+++ src/main/java/com/tele/maventest/testB.java
@@ -4,5 +4,6 @@ public class testB {
 public static void main(String[] args) {
 	System.out.println("this is testB");
 	System.out.println("继续测试！");
+	System.out.println("项目已经上传到github上了，现在测试是否能将这条新加的上传");
 }
 }
